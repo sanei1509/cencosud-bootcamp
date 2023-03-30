558f363cec45ef0c0141c4f621482b7c98441054
Santiago Neira <santiagoneira52@gmail.com>
2023-03-30 18:43:56 -0300

    update pokedex project, interact with mongodb

--- pokedex-app/src/app.module.ts
+++ pokedex-app/src/app.module.ts
@@ -4,9 +4,12 @@ import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    // conexion a mongodb database
+    MongooseModule.forRoot('mongodb://localhost/nest'),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
