01d62c6106470e86d62d73bfe84a53009f5405b7
Santiago Neira <santiagoneira52@gmail.com>
2023-04-03 18:42:54 -0300

    borrando comentarios basura

--- GraphQL/reclamos-app/src/auth/auth.resolver.ts
+++ GraphQL/reclamos-app/src/auth/auth.resolver.ts
@@ -45,30 +45,4 @@ export class AuthResolver {
     throw new Error("Method not implemented.");
   }
 
-
-
-  // @Mutation(() => Auth)
-  // createAuth(@Args('createAuthInput') createAuthInput: CreateAuthInput) {
-  //   return this.authService.create(createAuthInput);
-  // }
-
-  // @Query(() => [Auth], { name: 'auth' })
-  // findAll() {
-  //   return this.authService.findAll();
-  // }
-
-  // @Query(() => Auth, { name: 'auth' })
-  // findOne(@Args('id', { type: () => Int }) id: number) {
-  //   return this.authService.findOne(id);
-  // }
-
-  // @Mutation(() => Auth)
-  // updateAuth(@Args('updateAuthInput') updateAuthInput: UpdateAuthInput) {
-  //   return this.authService.update(updateAuthInput.id, updateAuthInput);
-  // }
-
-  // @Mutation(() => Auth)
-  // removeAuth(@Args('id', { type: () => Int }) id: number) {
-  //   return this.authService.remove(id);
-  // }
 }
